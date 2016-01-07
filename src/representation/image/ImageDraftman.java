293d07cd04d47ac71d8ea20a46c53363e4cfc77d
Mayora13 <elegost81@hotmail.fr>
2016-01-07 12:46:38 +0100

    again
    and again

--- src/representation/image/ImageDraftman.java
+++ src/representation/image/ImageDraftman.java
@@ -99,5 +99,21 @@ public class ImageDraftman {
 		drawGraphics(a);
 		generateImage();
 	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
 	
 }
